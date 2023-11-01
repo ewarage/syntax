9a21cb60f94c040d24f8ca6f0d86407a203ba321
ewarage <damirgyy123@gmail.com>
2023-11-01 20:36:38 +0900

    Добавили 2_var

---
+++ 2_var.py
@@ -0,0 +1,49 @@
+#однострочный комментарий 
+
+'''
+многострочный комментарий
+asdasd
+asdasd
+asdasdasd
+'''
+
+"""
+asdasd
+asdasda
+asdasdas
+"""
+
+# Переменные:
+
+# snake_case (змеиная_нотация)
+var_1 = 12
+number_of_apples = 100
+
+# camelCase (верблюжьяНотация)
+myNumVar = 20
+
+# Константные переменные(константы)
+CONST_VAR = 1000
+
+
+# Встроенная функция вывода в терминал
+#print(var_1)
+#print(myNumVar)
+
+#print(myNumVar, CONST_VAR, number_of_apples)
+
+# Встроенная функция ввода из терминала
+#in_1 = input("Введите число:")
+#print(in_1) 
+
+# Небольшой калькулятор
+in_1 = input("Введите 1 число:")
+in_2 = input("Введите 2 число:")
+
+# конкатенация (объединение) строк
+#result = in_1 + in_2
+
+# Сложение чисел
+result = int(in_1) + int(in_2)
+
+print(result)
